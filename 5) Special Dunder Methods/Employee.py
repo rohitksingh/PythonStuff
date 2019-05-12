29cd47fe06c5bee8c7f84e9354b926507a900709
Rohit <r4rohit002@gmail.com>
2019-05-12 12:05:08 +0530

    Added repr and str dunder methods

--- 5) Special Dunder Methods/Employee.py
+++ 5) Special Dunder Methods/Employee.py
@@ -16,9 +16,20 @@ class Employee:
         test = self.fullname()
         return len(test)
 
+    def __repr__(self):
+        return "{} - {}".format(self.first, self.pay)
+
+    def __str__(self):
+        return "Employee Details " \
+               "\n------------------" \
+               "\nName:{} {} \nPay:{}" \
+               "\n------------------".format(self.first,self.last, self.pay)
+
 
 emp2 = Employee('Jon', 'Snow', 500)
 emp1 = Employee('Rohit', 'Singh', 10000)
 print(emp1 + emp2)
 print(len(emp1))
-
+print(emp1)
+print(str(emp2))
+print(repr(emp2))
